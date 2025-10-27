b0ebd19f066c3e4ff250b06423795270e8b16e75
15101062585 <15101062585@163.com>
2025-10-27 16:04:24 +0800

    提交

--- day12/erc20-indexer/src/main/java/com/erc20/indexer/config/CorsConfig.java
+++ day12/erc20-indexer/src/main/java/com/erc20/indexer/config/CorsConfig.java
@@ -1,16 +1,14 @@
 package com.erc20.indexer.config;
 
-import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import lombok.extern.slf4j.Slf4j;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         // 暂时移除日志语句
+        
         // log.info("Configuring CORS mappings");
         
         registry.addMapping("/api/**")
